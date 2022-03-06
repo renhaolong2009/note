fbd4e7399aa12b7ea2f1d6367737559e9d3d0c98
renchaolong <813979489@qq.com>
2022-03-06 14:31:05 +0800

    vsfb

--- csp-j/a.cpp
+++ csp-j/a.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int a;
     char b;
-    cin>>a>>b;
-    if(a <= 1000){
-        cout<<"8";//如果他小于1000克直接返回基础价
+    cin >> a >> b;
+    if (a <= 1000)
+    {
+        cout << "8"; //如果他小于1000克直接返回基础价
         return 0;
     }
     int x = 0;
     int sum;
     int q;
-    if(a > 1000){
-        x = a - 1000;
-        sum = x/500+1;
-        q = sum*4+8;
-    }
-    if(b == 'n')
+    if (a > 1000)
     {
-        cout<<q;
+        x = a - 1000;
+        sum = x / 500 + 1;
+        q = sum * 4 + 8;
+    }
+    if (b == 'n')
+    {
+        cout << q;
         return 0;
     }
 }
---
+++ csp-j/b.md
@@ -0,0 +1,41 @@
+- [信息学奥赛一本通](#信息学奥赛一本通)
+  - [2018次都是v](#2018次都是v)
+# 信息学奥赛一本通
+## 2018次都是v
+次哇
+vieu
+vdfv
+
+
+
+
+<span style="color:red;">
+asfasdf
+</span>
+
+| a    |   b   |    c |a|
+| :--- | :---: | ---: |---:|
+| a    |   b   |    C |d|
+| a    |   b   |    C |c|
+
+1. cdsvv
+    1. vfdd
+    2. vsv
+
+2. v
+3. ds
+4. vs
+
+```c++
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout"dscvf b";
+}
+
+``` 
+```python
+
+
+``` 
