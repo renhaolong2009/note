20536b12a4a5943d37df21fe41d6c4dbf548476b
renchaolong <813979489@qq.com>
2022-04-17 14:51:11 +0800

    归并排序

--- 数据结构与算法/03-Sorting-Advance/02-Merge-Sort/main.cpp
+++ 数据结构与算法/03-Sorting-Advance/02-Merge-Sort/main.cpp
@@ -7,13 +7,11 @@ using namespace std;
 template <typename T>
 void __merge(T arr[], int l, int mid, int r)
 {
-
     T aux[r - l + 1];
     for (int i = l; i <= r; i++)
     {
         aux[i - l] = arr[i];
     }
-
     int i = l, j = mid + 1;
     for (int k = l; k <= r; k++)
     {
@@ -38,43 +36,11 @@ void __merge(T arr[], int l, int mid, int r)
             j++;
         }
     }
-
-    /* T aux[r - l + 1];
-
-    for (int i = l; i <= r; i++)
-        aux[i - l] = arr[i];
-
-    int i = l, j = mid + 1;
-    for (int k = l; k <= r; k++)
-    {
-
-        if (i > mid)
-        {
-            arr[k] = aux[j - l];
-            j++;
-        }
-        else if (j > r)
-        {
-            arr[k] = aux[i - l];
-            i++;
-        }
-        else if (aux[i - l] < aux[j - l])
-        {
-            arr[k] = aux[i - l];
-            i++;
-        }
-        else
-        {
-            arr[k] = aux[j - l];
-            j++;
-        }
-    } */
 }
 
 template <typename T>
 void __mergeSort(T arr[], int l, int r)
 {
-
     if (l >= r)
         return;
     int mid = (l + r) / 2;
@@ -86,7 +52,6 @@ void __mergeSort(T arr[], int l, int r)
 template <typename T>
 void mergeSort(T arr[], int n)
 {
-    /* __mergeSort(arr, 0, n - 1); */
     __mergeSort(arr, 0, n - 1);
 }
 
