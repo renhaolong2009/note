76b957d49d5c0504f325b09739aa76d6c5800fc6
renchaolong <813979489@qq.com>
2022-04-30 16:43:31 +0800

    快速排序

--- 数据结构与算法/03-Sorting-Advance/05-Quick-Sort/main.cpp
+++ 数据结构与算法/03-Sorting-Advance/05-Quick-Sort/main.cpp
@@ -2,21 +2,21 @@
 #include <algorithm>
 #include "SortTestHelper.h"
 #include "MergeSort.h"
-#include "InsertionSort.h"
 using namespace std;
 template <typename T>
 int __partition(T arr[], int l, int r)
 {
-
     T v = arr[l];
 
-    int j = l; // arr[l+1...j] < v ; arr[j+1...i) > v
+    int j = l;
     for (int i = l + 1; i <= r; i++)
+    {
         if (arr[i] < v)
         {
+            swap(arr[j + 1], arr[i]);
             j++;
-            swap(arr[j], arr[i]);
         }
+    }
 
     swap(arr[l], arr[j]);
 
@@ -26,7 +26,6 @@ int __partition(T arr[], int l, int r)
 template <typename T>
 void __quickSort(T arr[], int l, int r)
 {
-
     if (l >= r)
         return;
 
@@ -38,23 +37,16 @@ void __quickSort(T arr[], int l, int r)
 template <typename T>
 void quickSort(T arr[], int n)
 {
-
     __quickSort(arr, 0, n - 1);
 }
-
 int main()
 {
-    int n = 1000000;
-    cout << "Test for random array, size = " << n << ", random range [0, " << n << "]" << endl;
-    int *arr1 = SortTestHelper::generateRandomArray(n, 0, n);
-    int *arr2 = SortTestHelper::copyIntArray(arr1, n);
+    int a[3] = {3, 2, 1};
+    quickSort(a, 3);
 
-    SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
-    SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
-
-    delete[] arr1;
-    delete[] arr2;
-
-    cout << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        cout << a[i] << " ";
+    }
     return 0;
 }
