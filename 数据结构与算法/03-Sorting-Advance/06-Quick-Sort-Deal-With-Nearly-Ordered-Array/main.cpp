3a3c069c6acb434df429cce6f8178786acb1b8c6
renchaolong <813979489@qq.com>
2022-04-30 20:16:11 +0800

    优化

--- 数据结构与算法/03-Sorting-Advance/06-Quick-Sort-Deal-With-Nearly-Ordered-Array/main.cpp
+++ 数据结构与算法/03-Sorting-Advance/06-Quick-Sort-Deal-With-Nearly-Ordered-Array/main.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 #include <algorithm>
+#include <ctime>
 #include "SortTestHelper.h"
 #include "MergeSort.h"
+#include "InsertionSort.h"
 using namespace std;
-
 template <typename T>
-int __partition(T arr[], int l, int r)
+int _partition(T arr[], int l, int r)
 {
-    swap(arr[l], arr[rand() % (r - l + 1) + 1]);
-    T v = arr[l];
+    swap(arr[l], arr[rand() % (r - l + 1) + l]);
 
+    T v = arr[l];
     int j = l;
     for (int i = l + 1; i <= r; i++)
-    {
         if (arr[i] < v)
         {
-            swap(arr[j + 1], arr[i]);
             j++;
+            swap(arr[j], arr[i]);
         }
-    }
 
     swap(arr[l], arr[j]);
 
@@ -26,28 +25,31 @@ int __partition(T arr[], int l, int r)
 }
 
 template <typename T>
-void __quickSort(T arr[], int l, int r)
+void _quickSort(T arr[], int l, int r)
 {
-    if (r - l <= 15) //同归并排序
+
+    if (r - l <= 15)
     {
         insertionSort(arr, l, r);
         return;
     }
 
-    int p = __partition(arr, l, r);
-    __quickSort(arr, l, p - 1);
-    __quickSort(arr, p + 1, r);
+    int p = _partition(arr, l, r);
+    _quickSort(arr, l, p - 1);
+    _quickSort(arr, p + 1, r);
 }
 
 template <typename T>
 void quickSort(T arr[], int n)
 {
+
     srand(time(NULL));
-    __quickSort(arr, 0, n - 1);
+    _quickSort(arr, 0, n - 1);
 }
+
 int main()
 {
-    int a[3] = {8, 654, 99};
+    int a[3] = {3, 2, 1};
     quickSort(a, 3);
     for (int i = 0; i < 3; i++)
     {
